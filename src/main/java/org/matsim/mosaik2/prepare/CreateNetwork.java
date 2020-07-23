1339d235d2cf26f121a2f9154f3d78994e189705
Janek Laudan <laudan@tu-berlin.de>
2020-07-23 11:59:47 +0200

    Add create network script

--- src/main/java/org/matsim/mosaik2/prepare/CreateNetwork.java
+++ src/main/java/org/matsim/mosaik2/prepare/CreateNetwork.java
@@ -1,4 +1,54 @@
 package org.matsim.mosaik2.prepare;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.prep.PreparedGeometry;
+import org.locationtech.jts.geom.prep.PreparedGeometryFactory;
+import org.matsim.api.core.v01.network.NetworkWriter;
+import org.matsim.contrib.osm.networkReader.SupersonicOsmNetworkReader;
+import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+
 public class CreateNetwork {
+
+	private static final CoordinateTransformation transformation = TransformationFactory.getCoordinateTransformation("EPSG:4326", "EPSG:25832");
+
+	public static void main(String[] args) {
+
+		var arguments = new InputArgs();
+		JCommander.newBuilder().addObject(arguments).build().parse(args);
+
+		var bounds = CreateNetwork.getBounds();
+
+		var network = new SupersonicOsmNetworkReader.Builder()
+				.setCoordinateTransformation(transformation)
+				.setIncludeLinkAtCoordWithHierarchy((coord, id) -> bounds.covers(MGC.coord2Point(coord)))
+				.build()
+				.read(arguments.inputFile);
+
+		new NetworkWriter(network).write(arguments.outputFile);
+	}
+
+	private static PreparedGeometry getBounds() {
+
+		var gFactory = new GeometryFactory();
+		var geometry = gFactory.createPolygon(new Coordinate[]{
+				new Coordinate(0, 0)
+		});
+
+		var pFactory = new PreparedGeometryFactory();
+		return pFactory.create(geometry);
+	}
+
+	private static class InputArgs {
+
+		@Parameter(names = {"-input"}, required = true)
+		String inputFile = "";
+
+		@Parameter(names = {"-output"}, required = true)
+		String outputFile = "";
+	}
 }

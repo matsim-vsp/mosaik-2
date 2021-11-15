ca078c15c395608057b099a6b811ff743b594209
Janek Laudan <laudan@tu-berlin.de>
2021-11-15 16:37:27 +0100

    figure out geotools geojson support. First step towards outputting stuff for kepler.gl

--- src/main/java/org/matsim/mosaik2/agentEmissions/AgentEmissionNetCdfReader.java
+++ src/main/java/org/matsim/mosaik2/agentEmissions/AgentEmissionNetCdfReader.java
@@ -2,12 +2,29 @@ package org.matsim.mosaik2.agentEmissions;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import io.jawg.geojson.Position;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.csv.CSVFormat;
+import org.geotools.data.DataUtilities;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.SchemaException;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeImpl;
+import org.geotools.geojson.feature.FeatureJSON;
+import org.geotools.geojson.geom.GeometryJSON;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.CoordinateXYZM;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LineString;
 import org.matsim.api.core.v01.Id;
 import org.matsim.vehicles.Vehicle;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.FeatureType;
 import ucar.ma2.*;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.Variable;
@@ -36,9 +53,9 @@ public class AgentEmissionNetCdfReader {
         String outputFile;
     }
     
-    public static void main(String... args) {
+    public static void main(String... args) throws SchemaException, IOException {
 
-        var input = new Input();
+    /*    var input = new Input();
         JCommander.newBuilder().addObject(input).build().parse(args);
         log.info("translating from netcdf to csv");
 
@@ -48,6 +65,10 @@ public class AgentEmissionNetCdfReader {
                 input.outputFile
         );
         log.info("Done.");
+
+     */
+
+        translateToGeoJson();
     }
 
     public static Set<String> readToRecord(String filename, String indexFilename) {
@@ -87,6 +108,32 @@ public class AgentEmissionNetCdfReader {
             throw new RuntimeException(e);
         }
         log.info("Done translating.");
+    }
+
+    public static void translateToGeoJson() throws SchemaException, IOException {
+
+        SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+        builder.add("agentId", String.class);
+        builder.add("geometry", LineString.class);
+        builder.setDefaultGeometry("geometry");
+        builder.setName("feature-type-name");
+
+        var TYPE = builder.buildFeatureType();
+        var geometry = new GeometryFactory().createLineString(new Coordinate[] {
+                new CoordinateXYZM(0,0,0,0),
+                new CoordinateXYZM(10, 10, 0, 20),
+                new CoordinateXYZM(50,50,0,30),
+        });
+
+        var featureBuilder = new SimpleFeatureBuilder(TYPE);
+        featureBuilder.set("agentId", "test-id");
+        featureBuilder.set("geometry", geometry);
+        var feature = featureBuilder.buildFeature("test-feature-id");
+        var featureCollection = new DefaultFeatureCollection();
+        featureCollection.add(feature);
+
+        var bla = new FeatureJSON();
+        bla.writeFeatureCollection(featureCollection, "C:/Users/Janek/Desktop/test.json");
     }
 
     public static void read(String filename, String indexFilename, Consumer<AgentEmissionRecord> consumer) {

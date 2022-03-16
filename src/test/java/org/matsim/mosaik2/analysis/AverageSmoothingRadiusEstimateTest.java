c18e0cd97ff34337b72b11ba31b5475965cf28ff
Janek Laudan <laudan@tu-berlin.de>
2022-03-16 16:25:36 +0100

    Prepare cut out smaller area for testing the computation.

--- src/test/java/org/matsim/mosaik2/analysis/AverageSmoothingRadiusEstimateTest.java
+++ src/test/java/org/matsim/mosaik2/analysis/AverageSmoothingRadiusEstimateTest.java
@@ -56,20 +56,25 @@ public class AverageSmoothingRadiusEstimateTest {
 
         log.info("loading palm data.");
         // load the 8 o clock time slice
-        var palmOutput = PalmOutputReader.read("C:/Users/Janekdererste/repos/shared-svn/projects/mosaik-2/data/berlin-chemistry-driver-results/run_B2_chem_w3_lod2_masked_M01.merged.nc", 8 * 4, 8 * 4);
+        var palmOutput = PalmOutputReader.read("C:/Users/janek/repos/shared-svn/projects/mosaik-2/data/berlin-chemistry-driver-results/run_B2_chem_w3_lod2_masked_M01.merged.nc", 8 * 4, 8 * 4);
         // take the pm10 raster
         var pm10Raster = palmOutput.getTimeBins().iterator().next().getValue().get("PM");
+
+        // cut 200x200 m in the center of the area.
+        var smallBounds = new Raster.Bounds(pm10Raster.getBounds().getMinX() + 800, pm10Raster.getBounds().getMinY() + 800, pm10Raster.getBounds().getMaxX() - 800, pm10Raster.getBounds().getMaxY() - 800);
+        var smallPm10Raster = new Raster(smallBounds, pm10Raster.getCellSize());
+        smallPm10Raster.setValueForEachIndex(pm10Raster::getValueByIndex);
 
         log.info("Finished loading palm data");
         log.info("Start loading Network.");
         // collect emissions
-        var network = NetworkUtils.readNetwork("C:\\Users\\Janekdererste\\Desktop\\output-emissions-berlin-v5.5-10pct\\emissions-berlin-v5.5-10pct.output_network.xml.gz");
+        var network = NetworkUtils.readNetwork("C:\\Users\\janek\\Desktop\\output-berlin-5.5-emissions\\berlin-v5.5-10pct.output_network.xml.gz");
         var handler = new AggregateEmissionsByTimeHandler(network, Set.of(Pollutant.PM), 3600, 1000);
         var manager = EventsUtils.createEventsManager();
         manager.addHandler(handler);
 
         log.info("Start parsing emission events");
-        new EmissionEventsReader(manager).readFile("C:\\Users\\Janekdererste\\Desktop\\output-emissions-berlin-v5.5-10pct\\emissions-berlin-v5.5-10pct.output_only_001_emission_events.xml.gz");
+        new EmissionEventsReader(manager).readFile("C:\\Users\\janek\\Desktop\\output-berlin-5.5-emissions\\berlin-v5.5-10pct.output_events.xml.gz");
 
         log.info("Start converting collected emissions. Take time slice form 8am, filter all links without emissions");
         var emissionsByTime = handler.getTimeBinMap();
@@ -80,7 +85,7 @@ public class AverageSmoothingRadiusEstimateTest {
                 .collect(Collectors.toMap(AbstractObject2DoubleMap.BasicEntry::getKey, AbstractObject2DoubleMap.BasicEntry::getDoubleValue, Double::sum, Object2DoubleOpenHashMap::new));
 
         log.info("call collectR");
-        var rasterOfRs = AverageSmoothingRadiusEstimate.collectR(pm10Raster, emissions);
+        var rasterOfRs = AverageSmoothingRadiusEstimate.collectR(smallPm10Raster, emissions);
         log.info("after collectR");
         assertNotNull(rasterOfRs);
     }

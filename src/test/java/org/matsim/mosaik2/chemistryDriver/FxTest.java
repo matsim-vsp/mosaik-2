a8d40707ae7e110bbda93cca4f06df3dc9b6c282
Janekdererste <janek.laudan@mailbox.org>
2023-03-29 09:35:48 +0200

    wip2

--- src/test/java/org/matsim/mosaik2/chemistryDriver/FxTest.java
+++ src/test/java/org/matsim/mosaik2/chemistryDriver/FxTest.java
@@ -1,14 +1,22 @@
 package org.matsim.mosaik2.chemistryDriver;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
 
 public class FxTest {
 
+
+
     @Test
-    public void rasterLine() {
+    public void rasterLine() throws JsonProcessingException {
+
+        var mapper = new ObjectMapper();
 
         BufferedImage bufferedImage = new BufferedImage(
                 10, 10, BufferedImage.TYPE_INT_RGB
@@ -18,12 +26,31 @@ public class FxTest {
         context.setColor(Color.BLACK);
         context.fillRect(3, 5, 7, 5);
 
-        System.out.println(bufferedImage.getRaster().getDataBuffer().toString());
-
+        /*
         for (var i = 0; i < 10; i++) {
             for (var j = 0; j < 10; j++) {
-                System.out.println(bufferedImage.getRGB(i, j));
+                int[] result = bufferedImage.getRaster().getPixel(i, j, new int[3]);
+                System.out.println(mapper.writeValueAsString(result));
             }
         }
+
+         */
+
+        /*
+        var data = ((DataBufferInt) bufferedImage.getRaster().getDataBuffer()).getData();
+        for (var datum : data) {
+            System.out.println(datum);
+        }
+
+         */
+
+var bands = bufferedImage.getRaster().getNumBands();
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                System.out.println("(" + i + ", " + j + "): " + bufferedImage.getRGB(i, j));
+            }
+        }
+
+
     }
 }

5d0724e3e21f0ea704e2061ed7d0760d0905858a
janekdererste <janek.laudan@mailbox.org>
2024-11-07 09:23:49 +0100

    add some more plots for the 5th mosaik paper.

--- src/main/R/mosaik-02-02.R
+++ src/main/R/mosaik-02-02.R
@@ -1,10 +1,18 @@
 # add tidyverse and custom color palette which looks like google spread sheets
 library(tidyverse)
+library(patchwork)
+
 cbPalette <- c("#4285f4", "#ea4335", "#fbbc04", "#34a853", "#ff6d01", "#46bdc6", "#7baaf7", "#f07b72", "#fcd04f", "#71c287")
+scenario_color_palette <- c(
+  "Base case" = "#4285f4",
+  "Scenario 1" = "#34a853",
+  "Scenario 2" = "#ea4335",
+  "Scenario 3" = "#fbbc04"
+)
 
 get_name_1 <- function(value) {
   if (grepl(".base", value, fixed = TRUE)) {
-    "Basecase"
+    "Base case"
   } else if (grepl(".berlin", value, fixed = TRUE)) {
     "Scenario 2"
   } else if (grepl(".city", value, fixed = TRUE)) {
@@ -16,7 +24,7 @@ get_name_1 <- function(value) {
 
 get_name_2 <- function(value) {
   if (grepl("Base Case", value, fixed = TRUE)) {
-    "Basecase"
+    "Base case"
   } else if (grepl("Berlin", value, fixed = TRUE)) {
     "Scenario 2"
   } else if (grepl("City Center", value, fixed = TRUE)) {
@@ -27,26 +35,6 @@ get_name_2 <- function(value) {
     stop(paste("Unexpected value:", value, "only values which contain '.base', '.berlin', '.city' are supported", sep = " "))
   }
 }
-
-# ------------------ Print modal shares for different scenarios ---------------
-share_by_hour <- read_csv("/Users/janek/Documents/berlin_roadpricing/modal-split-hour.csv")
-only_car <- share_by_hour %>%
-  filter(time < 86400) %>%
-  filter(mode == "car") %>%
-  mutate(hour = time / 3600) %>%
-  mutate(scenario_name = sapply(name, get_name_2))
-
-p <- ggplot(only_car, aes(x = hour)) +
-  geom_line(aes(y = value, color = scenario_name)) +
-  ggtitle("Anzahl MiV-Fahrten im Berliner Stadtgebiet") +
-  labs(color = "Szenario") +
-  xlab("Stunde") +
-  ylab("") +
-  scale_color_manual(values = cbPalette) +
-  theme_light()
-
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/car-trips-berlin.pdf", width = 220, height = 118, units = "mm")
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/car-trips-berlin.png", width = 220, height = 118, units = "mm", dpi = 300)
 
 
 #---------------------- Concentration Plots start here -------------
@@ -73,6 +61,17 @@ joined_nox <- base_nox %>%
   mutate(scenario_name = sapply(name, get_name_1)) %>%
   mutate(NOx = NOx * 1e6)
 
+base_pm <- palm_base %>% select(hour, x, y, PM10)
+berlin_pm <- palm_berlin %>% select(hour, x, y, PM10)
+city_pm <- palm_city %>% select(hour, x, y, PM10)
+
+joined_pm <- base_pm %>%
+  inner_join(berlin_pm, by = c("hour", "x", "y"), suffix = c("", ".berlin")) %>%
+  inner_join(city_pm, by = c("hour", "x", "y"), suffix = c(".base", ".city")) %>%
+  pivot_longer(cols = c(ends_with(".berlin"), ends_with(".base"), ends_with(".city")), values_to = "PM",) %>%
+  mutate(scenario_name = sapply(name, get_name_1)) %>%
+  mutate(PM = PM * 1e6)
+
 #------------------------ write diff csv ------------------------------------------------------------
 diffs_nox <- berlin_nox %>%
   inner_join(city_nox, by = c("hour", "x", "y"), suffix = c(".berlin", ".city")) %>%
@@ -91,92 +90,150 @@ diff_nox_city <- diffs_nox %>%
 write_csv(diff_nox_city, "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/scenarios/rp-city/city-diff-nox.xyt.csv")
 
 #------------------------ create box plots for hourly emissions in base case -------------------------
+pm_factor <- 10
+
 base_all_no <- palm_base %>%
-  select(hour, x, y, NO, NO2, NOx) %>%
-  pivot_longer(cols = c(NO, NO2, NOx), names_to = "species", values_to = "concentrations") %>%
+  select(hour, x, y, NO, NO2, NOx, PM10) %>%
+  mutate(PM = PM10 * pm_factor) %>%
+  pivot_longer(cols = c(NO, NO2, NOx, PM), names_to = "species", values_to = "concentrations") %>%
   mutate(concentrations = concentrations * 1e6)
 
 p <- ggplot(base_all_no, aes(x = factor(hour), y = concentrations, color = factor(species))) +
   geom_boxplot(outlier.shape = NaN) +
-  ylim(0, 150) +
-  ggtitle("PALM-4U concentrations NOx [\u00B5g/m3]") +
+  ggtitle("PALM-4U concentrations") +
   labs(color = "Species") +
   xlab("Hour") +
   ylab("") +
+  scale_y_continuous(
+    limits = c(0, 120),
+    name = "NOx concentrations [\u00B5g/m3]",
+    sec.axis = sec_axis(~. / pm_factor, name = "PM concentrations [\u00B5g/m3]")
+  ) +
   scale_fill_manual(values = cbPalette) +
   scale_color_manual(values = cbPalette) +
-  theme_light()
+  theme_light(base_size = 8)
 p
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/box-nox-base.pdf", width = 220, height = 118, units = "mm")
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/box-nox-base.png", width = 220, height = 118, units = "mm", dpi = 600)
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-base.pdf", width = 183, height = 80, units = "mm")
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-base.png", width = 183, height = 80, units = "mm", dpi = 600)
 
-base_pm <- palm_base %>%
-  pivot_longer(cols = c(PM10), names_to = "species", values_to = "concentrations") %>%
-  mutate(concentrations = concentrations * 1e6)
-
-p <- ggplot(base_pm, aes(x = factor(hour), y = concentrations, color = factor(species))) +
-  geom_boxplot(outlier.shape = NaN) +
-  ylim(0, 7.5) +
-  ggtitle("PALM-4U concentrations PM10 [\u00B5g/m3]") +
-  labs(color = "Species") +
-  xlab("Hour") +
-  ylab("") +
-  scale_fill_manual(values = cbPalette) +
-  scale_color_manual(values = cbPalette) +
-  theme_light()
-p
-ggsave(
-  plot = p,
-  filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/box-pm-base.pdf",
-  width = 220,
-  height = 118,
-  units = "mm"
-)
-ggsave(
-  plot = p,
-  filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/box-pm-base.png",
-  width = 220,
-  height = 118,
-  units = "mm"
-)
+# base_pm <- palm_base %>%
+#   pivot_longer(cols = c(PM10), names_to = "species", values_to = "concentrations") %>%
+#   mutate(concentrations = concentrations * 1e6)
+#
+# p <- ggplot(base_pm, aes(x = factor(hour), y = concentrations, color = factor(species))) +
+#   geom_boxplot(outlier.shape = NaN) +
+#   ylim(0, 7.5) +
+#   ggtitle("PALM-4U concentrations PM10 [\u00B5g/m3]") +
+#   labs(color = "Species") +
+#   xlab("Hour") +
+#   ylab("") +
+#   scale_fill_manual(values = cbPalette) +
+#   scale_color_manual(values = cbPalette) +
+#   theme_light()
+# p
+# ggsave(
+#   plot = p,
+#   filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-pm-base.pdf",
+#   width = 220,
+#   height = 118,
+#   units = "mm"
+# )
+# ggsave(
+#   plot = p,
+#   filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-pm-base.png",
+#   width = 220,
+#   height = 118,
+#   units = "mm"
+# )
 
 #------------------------ create box plots for comparison of scenarios ------------------------ 
 
 p <- ggplot(joined_nox, aes(x = factor(hour), y = NOx, color = factor(scenario_name))) +
   geom_boxplot(outlier.shape = NaN) +
-  ylim(0, 150) +
+  ylim(0, 125) +
   ggtitle("NOx concentrations [\u00B5g/m3] by scenario") +
   labs(color = "Scenario") +
   xlab("Hour") +
   ylab("") +
-  scale_fill_manual(values = cbPalette) +
-  scale_color_manual(values = cbPalette) +
-  theme_light()
+  scale_fill_manual(values = scenario_color_palette) +
+  scale_color_manual(values = scenario_color_palette) +
+  theme_light(base_size = 8)
 p
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/box-nox-compare.pdf", width = 220, height = 118, units = "mm")
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/box-nox-compare.png", width = 220, height = 118, units = "mm")
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-nox-compare.pdf", width = 183, height = 80, units = "mm")
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-nox-compare.png", width = 183, height = 80, units = "mm")
 
-#------------------------ create emission mass plot ------------------------ 
-
-base_factors <- palm_base %>%
-  select(hour, x, y, NOx, PM10) %>%
-  mutate(mass_NOx = NOx * 10 * 10 * 10, mass_PM10 = PM10 * 10 * 10 * 10) %>%
-  group_by(hour) %>%
-  summarise(factor_NOx = sum(mass_NOx) / 26428.476, factor_PM10 = sum(mass_PM10) / 1228.3311) %>%
-  pivot_longer(cols = starts_with("factor_"), values_to = "factor", names_to = "species", names_prefix = "factor_")
-
-p <- ggplot(base_factors, aes(hour, factor, color = species)) +
-  geom_line() +
-  ggtitle("Normalized mass of pollutants") +
-  labs(color = "species") +
-  xlab("hour") +
+p <- ggplot(joined_pm, aes(x = factor(hour), y = PM, color = factor(scenario_name))) +
+  geom_boxplot(outlier.shape = NaN) +
+  ylim(0, 7.5) +
+  ggtitle("PM concentrations [\u00B5g/m3] by scenario") +
+  labs(color = "Scenario") +
+  xlab("Hour") +
   ylab("") +
-  scale_color_manual(values = cbPalette) +
-  scale_color_manual(values = cbPalette) +
+  scale_fill_manual(values = scenario_color_palette) +
+  scale_color_manual(values = scenario_color_palette) +
+  theme_light(base_size = 8)
+p
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-pm-compare.pdf", width = 183, height = 103, units = "mm")
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-pm-compare.png", width = 183, height = 103, units = "mm")
+
+nox_medians <- joined_nox %>%
+  group_by(hour, name) %>%
+  summarise(median(NOx))
+
+pm_medians <- joined_pm %>%
+  group_by(hour, name) %>%
+  summarise(median(PM))
+
+nox_diffs <- joined_nox %>%
+  pivot_wider(id_cols = c("hour", "x", "y"), names_from = name, values_from = NOx) %>%
+  group_by(hour) %>%
+  summarise(median_base = median(NOx.base), median_berlin = median(NOx.berlin), median_city = median(NOx.city)) %>%
+  mutate(diff_median_berlin = median_base - median_berlin, diff_median_city = median_base - median_city) %>%
+  select(hour, diff_median_berlin, diff_median_city) %>%
+  pivot_longer(cols = c("diff_median_berlin", "diff_median_city"), names_to = "name", values_to = "diff_to_base")
+
+p <- ggplot(nox_diffs, aes(x = hour, y = diff_to_base, color = name)) +
+  geom_line() +
+  geom_point() +
   theme_light()
 p
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/line-normalized-sums.pdf", width = 220, height = 118, units = "mm")
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/line-normalized-sums.png", width = 220, height = 118, units = "mm")
+
+nox_diffs_relative <- joined_nox %>%
+  pivot_wider(id_cols = c("hour", "x", "y"), names_from = name, values_from = NOx) %>%
+  group_by(hour) %>%
+  summarise(median_base = median(NOx.base), median_berlin = median(NOx.berlin), median_city = median(NOx.city)) %>%
+  mutate(diff_median_berlin = median_berlin / median_base, diff_median_city = median_city / median_base) %>%
+  select(hour, diff_median_berlin, diff_median_city) %>%
+  pivot_longer(cols = c("diff_median_berlin", "diff_median_city"), names_to = "name", values_to = "diff_to_base")
+
+p <- ggplot(nox_diffs_relative, aes(x = hour, y = diff_to_base, color = name)) +
+  geom_line() +
+  geom_point() +
+  theme_light()
+p
+#------------------------ create emission mass plot ------------------------
+
+# skip this one for the journal paper
+
+# base_factors <- palm_base %>%
+#   select(hour, x, y, NOx, PM10) %>%
+#   mutate(mass_NOx = NOx * 10 * 10 * 10, mass_PM10 = PM10 * 10 * 10 * 10) %>%
+#   group_by(hour) %>%
+#   summarise(factor_NOx = sum(mass_NOx) / 26428.476, factor_PM10 = sum(mass_PM10) / 1228.3311) %>%
+#   pivot_longer(cols = starts_with("factor_"), values_to = "factor", names_to = "species", names_prefix = "factor_")
+#
+# p <- ggplot(base_factors, aes(hour, factor, color = species)) +
+#   geom_line() +
+#   ggtitle("Normalized mass of pollutants") +
+#   labs(color = "species") +
+#   xlab("hour") +
+#   ylab("") +
+#   scale_color_manual(values = cbPalette) +
+#   scale_color_manual(values = cbPalette) +
+#   theme_light()
+# p
+# ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/line-normalized-sums.pdf", width = 220, height = 118, units = "mm")
+# ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/line-normalized-sums.png", width = 220, height = 118, units = "mm")
 
 #------------------------ create toll plot ------------------------ 
 
@@ -187,17 +244,70 @@ base_toll <- palm_base %>%
   summarise(factor_NOx = sum(mass_NOx) / 26428.476, factor_PM10 = sum(mass_PM10) / 1228.3311) %>%
   mutate(toll_NOx = factor_NOx * 36.8 / 1000 * 0.338 / 1000, toll_PM10 = factor_PM10 * 36.8 / 1000 * 0.002 / 1000) %>%
   mutate(total_toll = toll_NOx + toll_PM10) %>%
-  mutate(total_toll = total_toll * 100)
+  # we increased the toll by a factor of 100 and then we want it per km
+  mutate(total_toll = total_toll * 100 * 1000)
 
-p <- ggplot(base_toll, aes(hour, total_toll)) +
+p_toll <- ggplot(base_toll, aes(hour, total_toll)) +
   geom_line(color = "#4285f4") +
-  ggtitle("Charged toll by the hour [EUR/m]") +
+  geom_point(color = "#4285f4", size = 0.75) +
+  ggtitle("Charged toll by the hour [EUR/km]") +
   xlab("Hour") +
   ylab("") +
+  theme_light(base_size = 8)
+p_toll
+# ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/line-toll.pdf", width = 140, height = 79, units = "mm")
+# ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/line-toll.png", width = 140, height = 79, units = "mm")
+
+# ------------------ Print modal shares for different scenarios ---------------
+share_by_hour <- read_csv("/Users/janek/Documents/berlin_roadpricing/modal-split-hour.csv")
+only_car <- share_by_hour %>%
+  filter(time < 86400) %>%
+  filter(mode == "car") %>%
+  mutate(hour = time / 3600) %>%
+  mutate(scenario_name = sapply(name, get_name_2))
+
+p_modal_share <- ggplot(only_car, aes(x = hour, y = value, color = scenario_name)) +
+  geom_line() +
+  geom_point(size = 0.75) +
+  ggtitle("Car trips within Berlin City Center") +
+  labs(color = "Scenario") +
+  xlab("Hour") +
+  ylab("") +
+  scale_color_manual(values = scenario_color_palette) +
+  theme_light(base_size = 8)
+p_modal_share
+
+p_combined <- p_toll + p_modal_share
+ggsave(p_combined, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/toll_modal_share_combined.pdf", width = 183, height = 80, units = "mm")
+ggsave(p_combined, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/toll_modal_share_combined.png", width = 183, height = 80, units = "mm", dpi = 300)
+
+# Include diff plots of the car trips, to retreive numbers for the written report
+car_diffs_relative <- only_car %>%
+  pivot_wider(id_cols = c("time", "hour"), names_from = scenario_name, values_from = value) %>%
+  mutate(diff_scenario_1 = `Scenario 1` / `Base case`) %>%
+  mutate(diff_scenario_2 = `Scenario 2` / `Base case`) %>%
+  mutate(diff_scenario_3 = `Scenario 3` / `Base case`) %>%
+  select(time, hour, diff_scenario_1, diff_scenario_2, diff_scenario_3) %>%
+  pivot_longer(cols = c("diff_scenario_1", "diff_scenario_2", "diff_scenario_3"), names_to = "name", values_to = "value")
+
+p <- ggplot(car_diffs_relative, aes(x = hour, y = value, color = name)) +
+  geom_line() +
   theme_light()
 p
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/line-toll.pdf", width = 220, height = 118, units = "mm")
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/line-toll.png", width = 220, height = 118, units = "mm")
+
+car_diffs_absolute <- only_car %>%
+  pivot_wider(id_cols = c("time", "hour"), names_from = scenario_name, values_from = value) %>%
+  mutate(diff_scenario_1 = `Base case` - `Scenario 1`) %>%
+  mutate(diff_scenario_2 = `Base case` - `Scenario 2`) %>%
+  mutate(diff_scenario_3 = `Base case` - `Scenario 3`) %>%
+  select(time, hour, diff_scenario_1, diff_scenario_2, diff_scenario_3) %>%
+  pivot_longer(cols = c("diff_scenario_1", "diff_scenario_2", "diff_scenario_3"), names_to = "name", values_to = "value")
+
+p <- ggplot(car_diffs_absolute, aes(x = hour, y = value, color = name)) +
+  geom_line() +
+  geom_point() +
+  theme_light()
+p
 
 #----------------------- create linear fit plot
 # read in smoothed matsim data
@@ -223,7 +333,10 @@ p <- ggplot(matsim_palm_base, aes(NOx.matsim, y = NOx.palm)) +
   ggtitle("NOx concentrations [\u00B5g/m3] MATSim vs. PALM-4U") +
   facet_wrap(vars(hour)) +
   scale_fill_manual(values = cbPalette) +
-  theme_light()
+  theme_light(base_size = 8) +
+  theme(
+    strip.text = element_text(size = 6, margin = margin(t = 2, b = 2))
+  )
 p
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/linear-fit-nox.pdf", width = 220, height = 118, units = "mm")
-ggsave(plot = p, filename = "/Users/janek/repos/shared-svn/projects/mosaik-2/doc/40_publications/mosaik-2-02/data-files/r-output/linear-fit-nox.png", width = 220, height = 118, units = "mm")
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/linear-fit-nox.pdf", width = 183, height = 103, units = "mm")
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/linear-fit-nox.png", width = 183, height = 103, units = "mm")

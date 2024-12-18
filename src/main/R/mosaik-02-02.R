279e2618be944ca3347b7c172f850433cf35c05b
janekdererste <janek.laudan@mailbox.org>
2024-12-18 08:56:26 +0100

    Use onlz scenario A and B

--- src/main/R/mosaik-02-02.R
+++ src/main/R/mosaik-02-02.R
@@ -5,18 +5,18 @@ library(patchwork)
 cbPalette <- c("#4285f4", "#ea4335", "#fbbc04", "#34a853", "#ff6d01", "#46bdc6", "#7baaf7", "#f07b72", "#fcd04f", "#71c287")
 scenario_color_palette <- c(
   "Base case" = "#4285f4",
-  "Scenario 1" = "#34a853",
-  "Scenario 2" = "#ea4335",
-  "Scenario 3" = "#fbbc04"
+  "Scenario C" = "#34a853",
+  "Scenario B" = "#ea4335",
+  "Scenario A" = "#fbbc04"
 )
 
 get_name_1 <- function(value) {
   if (grepl(".base", value, fixed = TRUE)) {
     "Base case"
   } else if (grepl(".berlin", value, fixed = TRUE)) {
-    "Scenario 2"
+    "Scenario B"
   } else if (grepl(".city", value, fixed = TRUE)) {
-    "Scenario 3"
+    "Scenario A"
   } else {
     stop(paste("Unexpected value:", value, "only values which contain '.base', '.berlin', '.city' are supported", sep = " "))
   }
@@ -26,11 +26,11 @@ get_name_2 <- function(value) {
   if (grepl("Base Case", value, fixed = TRUE)) {
     "Base case"
   } else if (grepl("Berlin", value, fixed = TRUE)) {
-    "Scenario 2"
+    "Scenario B"
   } else if (grepl("City Center", value, fixed = TRUE)) {
-    "Scenario 3"
+    "Scenario A"
   } else if (grepl("All", value, fixed = TRUE)) {
-    "Scenario 1"
+    "Scenario C"
   } else {
     stop(paste("Unexpected value:", value, "only values which contain '.base', '.berlin', '.city' are supported", sep = " "))
   }
@@ -60,6 +60,17 @@ joined_nox <- base_nox %>%
   pivot_longer(cols = c(ends_with(".berlin"), ends_with(".base"), ends_with(".city")), values_to = "NOx",) %>%
   mutate(scenario_name = sapply(name, get_name_1)) %>%
   mutate(NOx = NOx * 1e6)
+
+base_no2 <- palm_base %>% select(hour, x, y, NO2)
+berlin_no2 <- palm_berlin %>% select(hour, x, y, NO2)
+city_no2 <- palm_city %>% select(hour, x, y, NO2)
+
+joined_no2 <- base_no2 %>%
+  inner_join(berlin_no2, by = c("hour", "x", "y"), suffix = c("", ".berlin")) %>%
+  inner_join(city_no2, by = c("hour", "x", "y"), suffix = c(".base", ".city")) %>%
+  pivot_longer(cols = c(ends_with(".berlin"), ends_with(".base"), ends_with(".city")), values_to = "NO2",) %>%
+  mutate(scenario_name = sapply(name, get_name_1)) %>%
+  mutate(NO2 = NO2 * 1e6)
 
 base_pm <- palm_base %>% select(hour, x, y, PM10)
 berlin_pm <- palm_berlin %>% select(hour, x, y, PM10)
@@ -116,36 +127,6 @@ p
 ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-base.pdf", width = 183, height = 80, units = "mm")
 ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-base.png", width = 183, height = 80, units = "mm", dpi = 600)
 
-# base_pm <- palm_base %>%
-#   pivot_longer(cols = c(PM10), names_to = "species", values_to = "concentrations") %>%
-#   mutate(concentrations = concentrations * 1e6)
-#
-# p <- ggplot(base_pm, aes(x = factor(hour), y = concentrations, color = factor(species))) +
-#   geom_boxplot(outlier.shape = NaN) +
-#   ylim(0, 7.5) +
-#   ggtitle("PALM-4U concentrations PM10 [\u00B5g/m3]") +
-#   labs(color = "Species") +
-#   xlab("Hour") +
-#   ylab("") +
-#   scale_fill_manual(values = cbPalette) +
-#   scale_color_manual(values = cbPalette) +
-#   theme_light()
-# p
-# ggsave(
-#   plot = p,
-#   filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-pm-base.pdf",
-#   width = 220,
-#   height = 118,
-#   units = "mm"
-# )
-# ggsave(
-#   plot = p,
-#   filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-pm-base.png",
-#   width = 220,
-#   height = 118,
-#   units = "mm"
-# )
-
 #------------------------ create box plots for comparison of scenarios ------------------------ 
 
 p <- ggplot(joined_nox, aes(x = factor(hour), y = NOx, color = factor(scenario_name))) +
@@ -161,6 +142,20 @@ p <- ggplot(joined_nox, aes(x = factor(hour), y = NOx, color = factor(scenario_n
 p
 ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-nox-compare.pdf", width = 183, height = 80, units = "mm")
 ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-nox-compare.png", width = 183, height = 80, units = "mm")
+
+p <- ggplot(joined_no2, aes(x = factor(hour), y = NO2, color = factor(scenario_name))) +
+  geom_boxplot(outlier.shape = NaN) +
+  ylim(0, 125) +
+  ggtitle("NO2 concentrations [\u00B5g/m3] by scenario") +
+  labs(color = "Scenario") +
+  xlab("Hour") +
+  ylab("") +
+  scale_fill_manual(values = scenario_color_palette) +
+  scale_color_manual(values = scenario_color_palette) +
+  theme_light(base_size = 8)
+p
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-no2-compare.pdf", width = 183, height = 80, units = "mm")
+ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/box-no2-compare.png", width = 183, height = 80, units = "mm")
 
 p <- ggplot(joined_pm, aes(x = factor(hour), y = PM, color = factor(scenario_name))) +
   geom_boxplot(outlier.shape = NaN) +
@@ -179,6 +174,19 @@ ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-fil
 nox_medians <- joined_nox %>%
   group_by(hour, name) %>%
   summarise(median(NOx))
+
+no2_medians <- joined_no2 %>%
+  group_by(hour, name) %>%
+  summarise(median(NO2))
+
+no2_diffs_relative <- joined_no2 %>%
+  pivot_wider(id_cols = c("hour", "x", "y"), names_from = name, values_from = NO2) %>%
+  group_by(hour) %>%
+  summarise(median_base = median(NO2.base), median_berlin = median(NO2.berlin), median_city = median(NO2.city)) %>%
+  mutate(diff_median_berlin = median_berlin / median_base, diff_median_city = median_city / median_base) %>%
+  select(hour, diff_median_berlin, diff_median_city) %>%
+  pivot_longer(cols = c("diff_median_berlin", "diff_median_city"), names_to = "name", values_to = "diff_to_base")
+
 
 pm_medians <- joined_pm %>%
   group_by(hour, name) %>%
@@ -264,7 +272,8 @@ only_car <- share_by_hour %>%
   filter(time < 86400) %>%
   filter(mode == "car") %>%
   mutate(hour = time / 3600) %>%
-  mutate(scenario_name = sapply(name, get_name_2))
+  mutate(scenario_name = sapply(name, get_name_2)) %>%
+  filter(scenario_name != "Scenario C")
 
 p_modal_share <- ggplot(only_car, aes(x = hour, y = value, color = scenario_name)) +
   geom_line() +
@@ -284,11 +293,11 @@ ggsave(p_combined, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-f
 # Include diff plots of the car trips, to retreive numbers for the written report
 car_diffs_relative <- only_car %>%
   pivot_wider(id_cols = c("time", "hour"), names_from = scenario_name, values_from = value) %>%
-  mutate(diff_scenario_1 = `Scenario 1` / `Base case`) %>%
-  mutate(diff_scenario_2 = `Scenario 2` / `Base case`) %>%
-  mutate(diff_scenario_3 = `Scenario 3` / `Base case`) %>%
-  select(time, hour, diff_scenario_1, diff_scenario_2, diff_scenario_3) %>%
-  pivot_longer(cols = c("diff_scenario_1", "diff_scenario_2", "diff_scenario_3"), names_to = "name", values_to = "value")
+  mutate(diff_scenario_c = `Scenario C` / `Base case`) %>%
+  mutate(diff_scenario_b = `Scenario B` / `Base case`) %>%
+  mutate(diff_scenario_a = `Scenario A` / `Base case`) %>%
+  select(time, hour, diff_scenario_a, diff_scenario_b, diff_scenario_c) %>%
+  pivot_longer(cols = c("diff_scenario_a", "diff_scenario_b", "diff_scenario_c"), names_to = "name", values_to = "value")
 
 p <- ggplot(car_diffs_relative, aes(x = hour, y = value, color = name)) +
   geom_line() +
@@ -297,11 +306,11 @@ p
 
 car_diffs_absolute <- only_car %>%
   pivot_wider(id_cols = c("time", "hour"), names_from = scenario_name, values_from = value) %>%
-  mutate(diff_scenario_1 = `Base case` - `Scenario 1`) %>%
-  mutate(diff_scenario_2 = `Base case` - `Scenario 2`) %>%
-  mutate(diff_scenario_3 = `Base case` - `Scenario 3`) %>%
-  select(time, hour, diff_scenario_1, diff_scenario_2, diff_scenario_3) %>%
-  pivot_longer(cols = c("diff_scenario_1", "diff_scenario_2", "diff_scenario_3"), names_to = "name", values_to = "value")
+  mutate(diff_scenario_c = `Base case` - `Scenario C`) %>%
+  mutate(diff_scenario_b = `Base case` - `Scenario B`) %>%
+  mutate(diff_scenario_a = `Base case` - `Scenario A`) %>%
+  select(time, hour, diff_scenario_a, diff_scenario_b, diff_scenario_c) %>%
+  pivot_longer(cols = c("diff_scenario_a", "diff_scenario_b", "diff_scenario_c"), names_to = "name", values_to = "value")
 
 p <- ggplot(car_diffs_absolute, aes(x = hour, y = value, color = name)) +
   geom_line() +
@@ -320,16 +329,19 @@ matsim_palm_base <- palm_base %>%
   filter(hour < 18) %>%
   mutate(NOx.matsim = NOx.matsim * 1e6) %>%
   mutate(NOx.palm = NOx.palm * 1e6) %>%
+  #filter(NOx.palm < 500 & NOx.matsim < 10000) %>%
   select(hour, x, y, NOx.matsim, NOx.palm)
 
 #Our transformation function
 scaleFUN <- function(x) sprintf("%.2f", x)
 
 p <- ggplot(matsim_palm_base, aes(NOx.matsim, y = NOx.palm)) +
-  geom_point(shape = ".") +
-  geom_smooth(method = "lm") +
-  ylim(0, 500) +
-  xlim(0, 10000) +
+  geom_point(shape = '.', alpha = 0.05, size = 0.1) +
+  geom_smooth(method = "lm", linewidth = 0.5) +
+  ylim(0, 400) +
+  xlim(0, 8000) +
+  xlab("MATSim") +
+  ylab("PALM-4U") +
   ggtitle("NOx concentrations [\u00B5g/m3] MATSim vs. PALM-4U") +
   facet_wrap(vars(hour)) +
   scale_fill_manual(values = cbPalette) +
@@ -337,7 +349,7 @@ p <- ggplot(matsim_palm_base, aes(NOx.matsim, y = NOx.palm)) +
   theme(
     strip.text = element_text(size = 6, margin = margin(t = 2, b = 2))
   )
-p
+#p
 ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/linear-fit-nox.pdf", width = 183, height = 103, units = "mm")
 ggsave(plot = p, filename = "/Users/janek/Documents/writing/mosaik-2-05/data-files/r-output/linear-fit-nox.png", width = 183, height = 103, units = "mm")
 
